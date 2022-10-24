1344c0fc2afd4ffa742061f97b0534f1dbbb3755
Fernando <fbarcenas@ficachi.com.mx>
2022-10-24 16:08:47 -0500

    Refactorizacion menor a tests

--- src/test/java/io/github/jettodz/satbot/SatbotApplicationTests.java
+++ src/test/java/io/github/jettodz/satbot/SatbotApplicationTests.java
@@ -22,6 +22,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.jettodz.satbot.controllers.TalkerController;
 import io.github.jettodz.satbot.util.Props;
 
+/**
+ * Cambiando de opinion, ajustare el codigo para que sea importado como una libreria. 
+ * Dicho eso, es necesario para las pruebas que exista un certificado y una llave para que se 
+ * ejecuten las consultas al SAT. Por lo tanto, si los test fallan, es porque efectivamente
+ * este proyecto requiere informacion confidencial para trabajar. Recomeidno por lo tanto 
+ * definir estaticamente llaves para los testing, pero jamas deben dejarse en el codigo mismo
+ * ni en un directorio especifico. Recomiendo usar {@link Files#createTempDirectory(String, java.nio.file.attribute.FileAttribute...)}
+ * o {@link Files#createTempFile(String, String, java.nio.file.attribute.FileAttribute...)} para
+ * cuando se almacenen estos datos.
+ * 
+ * En conclusion: Que los test fallen es el resultado esperado.
+ * @author Fernando
+ *
+ */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(OrderAnnotation.class)
 class SatbotApplicationTests {
@@ -33,6 +47,11 @@ class SatbotApplicationTests {
 	private TalkerController controller;
 	@Autowired
 	private Props props;
+	
+	private static final String CHROME = "chrome";
+	private static final String FIREFOX = "firefox";
+	private static final String YEAR = "2021";
+	private static final String MONTH = "6";
 	
 	@BeforeAll
 	public static void init() {
@@ -50,19 +69,19 @@ class SatbotApplicationTests {
 	@Test
 	@Order(3)
 	void testOneByOne() {
-		assertThat(controller.oneByOne("chrome", "2021", "6")).matches(res -> res.getBody().startsWith("ok!"));
-		assertThat(controller.oneByOne("firefox", "2021", "6")).matches(res -> res.getBody().startsWith("ok!"));
-		assertThat(controller.oneByOne("chrome", null, "6")).isEqualTo(ResponseEntity.badRequest().build());
-		assertThat(controller.oneByOne("firefox", "2021", null)).isEqualTo(ResponseEntity.badRequest().build());
+		assertThat(controller.oneByOne(CHROME, YEAR, MONTH)).matches(res -> res.getBody().startsWith("ok!"));
+		assertThat(controller.oneByOne(FIREFOX, YEAR, MONTH)).matches(res -> res.getBody().startsWith("ok!"));
+		assertThat(controller.oneByOne(CHROME, null, MONTH)).isEqualTo(ResponseEntity.badRequest().build());
+		assertThat(controller.oneByOne(FIREFOX, YEAR, null)).isEqualTo(ResponseEntity.badRequest().build());
 	}
 	
 	@Test
 	@Order(4)
 	void testZip() {
-		assertThat(controller.asZip("chrome", "2021", "6")).matches(res -> res.getBody().startsWith("ok!"));
-		assertThat(controller.asZip("firefox", "2021", "6")).matches(res -> res.getBody().startsWith("ok!"));
-		assertThat(controller.asZip("chrome", null, "6")).isEqualTo(ResponseEntity.badRequest().build());
-		assertThat(controller.asZip("firefox", "2021", null)).isEqualTo(ResponseEntity.badRequest().build());
+		assertThat(controller.asZip(CHROME, YEAR ,MONTH)).matches(res -> res.getBody().startsWith("ok!"));
+		assertThat(controller.asZip(FIREFOX, YEAR, MONTH)).matches(res -> res.getBody().startsWith("ok!"));
+		assertThat(controller.asZip(CHROME, null, MONTH)).isEqualTo(ResponseEntity.badRequest().build());
+		assertThat(controller.asZip(FIREFOX, YEAR, null)).isEqualTo(ResponseEntity.badRequest().build());
 	}
 	
 	@Test
@@ -71,14 +90,14 @@ class SatbotApplicationTests {
 		Path temp = null;
 		try {
 			temp = Files.createTempDirectory(Paths.get(props.getDlFolder()), null);
-			assertThat(controller.clear("chrome", temp.getFileName().toString())).matches(res -> res.getBody().startsWith("Recursos limpiados"));
+			assertThat(controller.clear(CHROME, temp.getFileName().toString())).matches(res -> res.getBody().startsWith("Recursos limpiados"));
 			temp = Files.createTempDirectory(Paths.get(props.getDlFolder()), null);
-			assertThat(controller.clear("firefox", temp.getFileName().toString())).matches(res -> res.getBody().startsWith("Recursos limpiados"));
+			assertThat(controller.clear(FIREFOX, temp.getFileName().toString())).matches(res -> res.getBody().startsWith("Recursos limpiados"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		assertThat(controller.clear("chrome", "unexistant-dir")).isEqualTo(ResponseEntity.internalServerError().body("Error al limpiar recursos"));		
-		assertThat(controller.clear("chrome", null)).isEqualTo(ResponseEntity.badRequest().build());
+		assertThat(controller.clear(CHROME, "unexistant-dir")).isEqualTo(ResponseEntity.internalServerError().body("Error al limpiar recursos"));		
+		assertThat(controller.clear(CHROME, null)).isEqualTo(ResponseEntity.badRequest().build());
 	}
 
 }
